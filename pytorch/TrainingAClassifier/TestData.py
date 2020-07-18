fed8c23fdbf74c5eda4491bdb9e4c5c57f1d2173
Zhang Bing <537506288@qq.com>
2020-07-19 02:19:09 +0800

    Add files via upload
    add

---
+++ pytorch/TrainingAClassifier/TestData.py
@@ -0,0 +1,101 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False, num_workers=0)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+def imshow(img):
+    img = img / 2 + 0.5  # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+# ======================================================
+# 重新加载训练过的网络，节省时间
+# ======================================================
+net = Net()
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# net.to(device)
+PATH = './cifar_net.pth'
+net.load_state_dict(torch.load(PATH))
+
+
+# ======================================================
+# 测试网络
+# ======================================================
+
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+
+# print images
+imshow(torchvision.utils.make_grid(images))
+print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+outputs = net(images)
+_, predicted = torch.max(outputs, 1)
+print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
+
+# ======================================================
+# 所以测试集一起测试准确率
+# ======================================================
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
+
+# ======================================================
+# 测试各类准确率
+# ======================================================
+class_correct = list(0. for i in range(10))
+class_total = list(0. for i in range(10))
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs, 1)
+        c = (predicted == labels).squeeze()
+        for i in range(4):
+            label = labels[i]
+            class_correct[label] += c[i].item()
+            class_total[label] += 1
+
+
+for i in range(10):
+    print('Accuracy of %5s : %2d %%' % (
+        classes[i], 100 * class_correct[i] / class_total[i]))
--- pytorch/TrainingAClassifier/readme.md
+++ pytorch/TrainingAClassifier/readme.md
@@ -2,7 +2,7 @@ Training a classifier
 ==============================================
 We will do the following steps in order:
 
-- Load and normalizing the CIFAR10 training and test datasets using > torchvision
+- Load and normalizing the CIFAR10 training and test datasets using torchvision
 - Define a Convolutional Neural Network
 - Define a loss function
 - Train the network on the training data
