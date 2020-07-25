fb31a022abe46d3a43ac6b1e9ff399964dca05f3
Mud-Fire <537506288@qq.com>
2020-07-25 22:13:47 +0800

    zhangbing

---
+++ pytorch/WhatIsTorch.NNReally/TorchNNTurorial.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+import requests
+import pickle
+import gzip
+
+DATA_PATH = Path("data")
+PATH = DATA_PATH / "mnist"
+
+PATH.mkdir(parents=True, exist_ok=True)
+
+URL = "http://deeplearning.net/data/mnist/"
+FILENAME = "mnist.pkl.gz"
+
+if not (PATH / FILENAME).exists():
+    content = requests.get(URL + FILENAME).content
+    (PATH / FILENAME).open("wb").write(content)
+
+with gzip.open((PATH / FILENAME).as_posix(), "rb") as f:
+    ((x_train, y_train), (x_valid, y_valid), _) = pickle.load(f, encoding="latin-1")
+
+from matplotlib import pyplot
+import numpy as np
+
+pyplot.imshow(x_train[0].reshape((28, 28)), cmap="gray")
+print(x_train.shape)
