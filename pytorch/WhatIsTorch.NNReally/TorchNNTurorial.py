a0a399d0ce443f1e1e46adf74f06dcda4633bff8
Mud-Fire <537506288@qq.com>
2020-07-25 22:20:19 +0800

    zhangbing

--- pytorch/WhatIsTorch.NNReally/TorchNNTurorial.py
+++ pytorch/WhatIsTorch.NNReally/TorchNNTurorial.py
@@ -2,7 +2,10 @@ from pathlib import Path
 import requests
 import pickle
 import gzip
+from matplotlib import pyplot
+import numpy as np
 
+# 准备数据
 DATA_PATH = Path("data")
 PATH = DATA_PATH / "mnist"
 
@@ -15,11 +18,10 @@ if not (PATH / FILENAME).exists():
     content = requests.get(URL + FILENAME).content
     (PATH / FILENAME).open("wb").write(content)
 
+# 获取数据
 with gzip.open((PATH / FILENAME).as_posix(), "rb") as f:
     ((x_train, y_train), (x_valid, y_valid), _) = pickle.load(f, encoding="latin-1")
 
-from matplotlib import pyplot
-import numpy as np
-
+# 查看数据
 pyplot.imshow(x_train[0].reshape((28, 28)), cmap="gray")
 print(x_train.shape)
