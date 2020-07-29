5e1142ce62cd09a232ce742f4d6d5a005b447d8d
Mud-Fire <537506288@qq.com>
2020-07-29 23:44:40 +0800

    update

--- pytorch/WhatIsTorch.NNReally/TorchNNTurorial.py
+++ pytorch/WhatIsTorch.NNReally/TorchNNTurorial.py
@@ -24,18 +24,55 @@ with gzip.open((PATH / FILENAME).as_posix(), "rb") as f:
     ((x_train, y_train), (x_valid, y_valid), _) = pickle.load(f, encoding="latin-1")
 
 # 查看数据
-pyplot.imshow(x_train[1].reshape((28, 28)), cmap="gray")
-pylab.show()  # pycharm 要用下pylab才能显示图像窗口
+# pyplot.imshow(x_train[1].reshape((28, 28)), cmap="gray")
+# pylab.show()  # pycharm 要用下pylab才能显示图像窗口
 print(x_train.shape)
 
 import torch
 
+print(type(x_train))
+# 把numpy的数组格式转化为torch.tensor
 x_train, y_train, x_valid, y_valid = map(
     torch.tensor, (x_train, y_train, x_valid, y_valid)
 )
 
 n, c = x_train.shape
-x_train, x_train.shape, y_train.min(), y_train.max()
+print(n, c)
+# x_train, x_train.shape, y_train.min(), y_train.max()
 print(x_train, y_train)
 print(x_train.shape)
 print(y_train.min(), y_train.max())
+
+import math
+
+weights = torch.randn(784, 10) / math.sqrt(784)
+print(weights.shape)
+weights.requires_grad_()
+bias = torch.zeros(10, requires_grad=True)
+
+
+def log_softmax(x):
+    return x - x.exp().sum(-1).log().unsqueeze(-1)
+
+
+def model(xb):
+    return log_softmax(xb @ weights + bias)
+
+
+bs = 64  # batch size
+
+xb = x_train[0:bs]  # a mini-batch from x
+preds = model(xb)  # predictions
+print(preds[0], preds.shape)
+
+def nll(input, target):
+    return -input[range(target.shape[0]), target].mean()
+
+loss_func = nll
+yb = y_train[0:bs]
+print(loss_func(preds, yb))
+
+def accuracy(out, yb):
+    preds = torch.argmax(out, dim=1)
+    return (preds == yb).float().mean()
+print(accuracy(preds, yb))
