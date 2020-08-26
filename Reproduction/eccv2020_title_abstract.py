3ce54af87fa4b77e094c36406fa08ab2efbffe8c
Mud-Fire <537506288@qq.com>
2020-08-26 23:24:29 +0800

    断点继续

--- Reproduction/eccv2020_title_abstract.py
+++ Reproduction/eccv2020_title_abstract.py
@@ -28,23 +28,29 @@ parttern = re.compile(r'papers/eccv_2020/papers_ECCV/html/\d*_ECCV_2020_paper.ph
 url_list = parttern.findall(html)
 print(len(url_list))
 
+
+
 paperTitle = []
 paperAbstract = []
 
-for i in range(0, len(url_list)):
+for i in range(484, len(url_list)):
     # name = url.split('/')[-1]
     # print(name)
-    if i % 100 == 0:
-        print(i)
+    print(i)
     sub_html = getHtml('https://www.ecva.net/' + url_list[i])
     p = pq(sub_html)
+    # print(p('div').filter('#papertitle').html().strip())
     paperTitle.append(p('div').filter('#papertitle').html().strip())
     paperAbstract.append(p('div').filter('#abstract').html().strip())
+    addList = (p('div').filter('#papertitle').html().strip(),p('div').filter('#abstract').html().strip())
+    print(addList)
+    with open(save_path, 'a', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(addList)
 
 print(len(paperTitle), len(paperAbstract))
 paperTable = list(zip(paperTitle, paperAbstract))
 
-with open(save_path, 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile)
-    for row in paperTable:
-        writer.writerow(row)
+# with open(save_path, 'a', newline='', encoding='utf-8') as csvfile:
+#     writer = csv.writer(csvfile)
+#     writer.writerows(paperTable)
