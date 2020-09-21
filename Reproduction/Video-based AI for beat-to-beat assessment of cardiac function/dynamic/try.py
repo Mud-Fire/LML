d2344e1fde93a3b33217e4aff0d665ff427c92b4
Mud-Fire <537506288@qq.com>
2020-09-21 11:53:08 +0800

    hh

--- Reproduction/Video-based AI for beat-to-beat assessment of cardiac function/dynamic/try.py
+++
@@ -1,7 +0,0 @@
-import echonet
-echonet.utils.segmentation.run(modelname="deeplabv3_resnet50",save_segmentation=True,pretrained=False)
-echonet.utils.video.run(modelname="r2plus1d_18",
-                                             frames=32,
-                                             period=2,
-                                             pretrained=True,
-                                             batch_size=8)
