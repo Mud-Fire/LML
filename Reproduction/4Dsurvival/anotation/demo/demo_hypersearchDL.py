7f22e19ee05d3bd10ae5fee7ab0fc396085d9be5
Mud-Fire <537506288@qq.com>
2020-09-29 22:20:00 +0800

    添加demo超参数运行后结果注释

--- Reproduction/4Dsurvival/anotation/demo/demo_hypersearchDL.py
+++ Reproduction/4Dsurvival/anotation/demo/demo_hypersearchDL.py
@@ -18,6 +18,7 @@ x_full = c3[0]
 y_full = c3[1]
 del c3
 
+
 # 寻找学习模型的超参数
 
 # x_data : 输入数据，网格点运动描述
@@ -31,31 +32,40 @@ del c3
 # units1_range : 编码器隐藏层 单元 数量
 # units2_range : 编码器 中间层 单元数量
 # alpha_range  : 公式（7） 重建损失和生存损失权重
-# batch_size   :
-# num_epochs   :
+# batch_size   : batch size
+# num_epochs   : 循环次数
 
-def hypersearch_DL(x_data, y_data, method, nfolds, nevals, lrexp_range, l1rexp_range, dro_range, 
+
+# 运行后结果:
+# lrexp : -5.0204296875
+# l1rexp: -5.868338333541976
+# dro   : 0.119903485594066286
+# units1: 144.62675497109615
+# units2: 17.858519854053874
+# alpha : 0.48721875
+# Cross-validated C after tuning: 0.782
+
+def hypersearch_DL(x_data, y_data, method, nfolds, nevals, lrexp_range, l1rexp_range, dro_range,
                    units1_range, units2_range, alpha_range, batch_size, num_epochs):
-
     @optunity.cross_validated(x=x_data, y=y_data, num_folds=nfolds)
     def modelrun(x_train, y_train, x_test, y_test, lrexp, l1rexp, dro, units1, units2, alpha):
-        cv_log = DL_single_run(xtr=x_train, ytr=y_train, units1=units1, units2=units2, dro=dro, lr=10**lrexp, 
-                               l1r=10**l1rexp, alpha=alpha, batchsize=batch_size, numepochs=num_epochs)
+        cv_log = DL_single_run(xtr=x_train, ytr=y_train, units1=units1, units2=units2, dro=dro, lr=10 ** lrexp,
+                               l1r=10 ** l1rexp, alpha=alpha, batchsize=batch_size, numepochs=num_epochs)
         cv_preds = cv_log.model.predict(x_test, batch_size=1)[1]
-        cv_C = concordance_index(y_test[:,1], -cv_preds, y_test[:,0])
+        cv_C = concordance_index(y_test[:, 1], -cv_preds, y_test[:, 0])
         return cv_C
 
-    optimal_pars, searchlog, _ = optunity.maximize(modelrun, num_evals=nevals, solver_name=method, lrexp=lrexp_range, 
-                                                   l1rexp=l1rexp_range, dro=dro_range, units1=units1_range, 
+    optimal_pars, searchlog, _ = optunity.maximize(modelrun, num_evals=nevals, solver_name=method, lrexp=lrexp_range,
+                                                   l1rexp=l1rexp_range, dro=dro_range, units1=units1_range,
                                                    units2=units2_range, alpha=alpha_range)
 
     print('Optimal hyperparameters : ' + str(optimal_pars))
     print('Cross-validated C after tuning: %1.3f' % searchlog.optimum)
     return optimal_pars, searchlog
-    
-    
-opars, clog = hypersearch_DL(x_data=x_full, y_data=y_full, 
+
+
+opars, clog = hypersearch_DL(x_data=x_full, y_data=y_full,
                              method='particle swarm', nfolds=6, nevals=50,
-                             lrexp_range=[-6.,-4.5], l1rexp_range=[-7,-4], dro_range=[.1,.9], 
-                             units1_range=[75,250],  units2_range=[5,20],  alpha_range=[0.3, 0.7],
+                             lrexp_range=[-6., -4.5], l1rexp_range=[-7, -4], dro_range=[.1, .9],
+                             units1_range=[75, 250], units2_range=[5, 20], alpha_range=[0.3, 0.7],
                              batch_size=16, num_epochs=100)
